9e54d1553396511323afdd4654912af45c8fb8e7
Nathan Fisher <nfisher@junctionbox.ca>
2019-06-24 12:05:19 -0300

    Move Workspace to bazel.io package
    refs #1

---
+++ src/mvntobzl/io/Workspace.kt
@@ -0,0 +1,55 @@
+package mvntobzl.io
+
+import java.io.IOException
+import java.nio.file.FileVisitResult
+import java.nio.file.FileVisitResult.CONTINUE
+import java.nio.file.FileVisitResult.SKIP_SUBTREE
+import java.nio.file.FileVisitor
+import java.nio.file.Files.walkFileTree
+import java.nio.file.Path
+import java.nio.file.attribute.BasicFileAttributes
+import java.util.concurrent.BlockingQueue
+
+fun walkWorkspace(start: Path, q: BlockingQueue<String>, ignoreFilePrefixes: List<String>) {
+    walkFileTree(start, WalkerTexasRanger(start, q, ignoreFilePrefixes))
+}
+
+class WalkerTexasRanger(private val start: Path, private val q: BlockingQueue<String>, val ignorePrefixes: List<String>) : FileVisitor<Path> {
+
+    override fun visitFile(file: Path, attrs: BasicFileAttributes?): FileVisitResult {
+        q.put(file.toString().substring(start.toString().length + 1))
+        return CONTINUE
+    }
+
+    override fun visitFileFailed(file: Path, exc: IOException?): FileVisitResult {
+        if (null != exc) {
+            throw exc
+        }
+        return CONTINUE
+    }
+
+    override fun preVisitDirectory(dir: Path, attrs: BasicFileAttributes?): FileVisitResult {
+        val filename = dir.fileName
+        for (ignorePrefix in ignorePrefixes) {
+            if (filename.toString().startsWith(ignorePrefix)) {
+                println("dotfile")
+                return SKIP_SUBTREE
+            }
+        }
+
+        return CONTINUE
+    }
+
+    override fun postVisitDirectory(dir: Path, exc: IOException?): FileVisitResult {
+        if (null != exc) {
+            throw exc
+        }
+        return CONTINUE
+    }
+}
+
+val IGNORE_FILE_PREFIXES = listOf(
+        "target",
+        "bazel-",
+        "."
+)
---
+++ test/mvntobzl/io/WorkspaceTestCase.kt
@@ -0,0 +1,74 @@
+package mvntobzl.io
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import java.io.File
+import java.nio.file.FileSystemException
+import java.nio.file.Files
+import java.nio.file.Path
+import java.util.concurrent.ArrayBlockingQueue
+import java.util.concurrent.BlockingQueue
+import java.util.stream.Collectors
+import kotlin.streams.asSequence
+import kotlin.test.assertEquals
+
+class WorkspaceTestCase {
+    val SIMPLE_PROJECT = listOf(
+            "pom.xml",
+            "metrics/pom.xml",
+            "metrics/src/main/java/com/instana/metrics/Formatter.java",
+            "metrics/src/main/java/com/instana/metrics/catalog/MetricDescription.java"
+    ).sorted()
+    val WITH_EXCLUDED_FILES = SIMPLE_PROJECT + listOf(
+            ".git/config",
+            "target/Formatter.class",
+            "bazel-out/Formatter.class"
+            )
+
+    var workspacePath: Path? = null
+
+    // timeout of 1s in case the queue put blocks indefinitely in walkWorkspace.
+    @Test(timeout = 1000L)
+    fun `ignore dotfiles while traversing`() {
+        val q = genWorkspace(workspacePath!!, WITH_EXCLUDED_FILES)
+
+        walkWorkspace(workspacePath!!, q, IGNORE_FILE_PREFIXES)
+
+        val result = q.stream().sorted().collect(Collectors.toList())
+        assertEquals(SIMPLE_PROJECT, result)
+    }
+
+    @Before
+    fun setUp() {
+        workspacePath = Files.createTempDirectory("junit_")
+    }
+
+    @After
+    fun tearDown() {
+        if (null == workspacePath) {
+            val path:Path = workspacePath!!
+            Files.walk(path)
+                    .asSequence()
+                    .sortedDescending()
+                    .map(Path::toFile)
+                    .forEach { it.delete() }
+        }
+    }
+}
+
+internal fun genWorkspace(workspacePath:Path, filenames:List<String>):BlockingQueue<String> {
+    for (filename in filenames) {
+        val f = File(workspacePath.toFile(), filename)
+        val dir = f.parent
+
+        if (null != dir) {
+            File(dir).mkdirs()
+        }
+
+        if (!f.createNewFile()) {
+            throw FileSystemException("unable to create file $filename")
+        }
+    }
+    return ArrayBlockingQueue(filenames.size + 1)
+}
